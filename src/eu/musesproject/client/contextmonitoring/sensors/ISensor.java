7d1cab752af7edd1b39d1cea5e2e4c8837922bb6
ChristophStanik <christophstanik@gmail.com>
2014-04-14 16:40:13 +0200

    Added a max size for the context event history list for each sensor and added a method to remove a ContextListener

--- src/eu/musesproject/client/contextmonitoring/sensors/ISensor.java
+++ src/eu/musesproject/client/contextmonitoring/sensors/ISensor.java
@@ -3,19 +3,35 @@ package eu.musesproject.client.contextmonitoring.sensors;
 import eu.musesproject.client.contextmonitoring.ContextListener;
 import eu.musesproject.contextmodel.ContextEvent;
 
-
 public interface ISensor {
-	/** This method enables the sensor. The sensor starts to collect data */
-	void enable();
-	
-	/** This method disables the sensor. The sensor stops to collect data and unregisters all broadcast receivers if there is one */
-	void disable();
-	
-	/**
-	 * register a listener to notify other components whenever a context event is fired
-	 * @param listener of type {@link ContextListener}
-	 */
-	void addContextListener(ContextListener listener);
-	
-	ContextEvent getLastFiredContextEvent();
+    /**
+     * max size of the array for the context event history
+     */
+    static final int CONTEXT_EVENT_HISTORY_SIZE = 2;
+
+
+    /**
+     * Must be called before a sensor is enabled
+     * register a listener to notify other components whenever a context event is fired
+     * @param listener of type {@link ContextListener}
+     */
+    void addContextListener(ContextListener listener);
+
+    /**
+     * Must be called before a sensor is disabled.
+     * unregister a listener
+     * @param listener of type {@link ContextListener}
+     */
+    void removeContextListener(ContextListener listener);
+
+    /** This method enables the sensor. The sensor starts to collect data */
+    void enable();
+
+    /** This method disables the sensor. The sensor stops to collect data and unregisters all broadcast receivers if there is one */
+    void disable();
+
+    /**
+     * @return the last fired context event
+     */
+    ContextEvent getLastFiredContextEvent();
 }
