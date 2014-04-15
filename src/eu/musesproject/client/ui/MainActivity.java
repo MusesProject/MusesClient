b2e8416c560fe3de86450aa96b352d9242099fe3
yasali <yasir.ali@swedenconnectivity.com>
2014-04-15 14:13:02 +0200

    new callback methods implemented in UI.

--- src/eu/musesproject/client/ui/MainActivity.java
+++ src/eu/musesproject/client/ui/MainActivity.java
@@ -6,19 +6,31 @@
 
 package eu.musesproject.client.ui;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import eu.musesproject.MUSESBackgroundService;
 import eu.musesproject.client.R;
+import eu.musesproject.client.contextmonitoring.UserContextMonitoringController;
+import eu.musesproject.client.contextmonitoring.service.aidl.Action;
+import eu.musesproject.client.model.decisiontable.ActionType;
 
 /**
  * MainActivity class handles List buttons on the main GUI
@@ -32,10 +44,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	private static String TAG = MainActivity.class.getSimpleName();
 	private LinearLayout topLayout;
 	private Button loginListBtn, securityInformationListbtn, privacyPolicyListBtn;
-	private static Context context;
+	private Context context;
 	private LoginView loginView;
 	private SecurityInformationView securityInformationView;
 	private PrivacyPolicyView privacyPolicyView;
+	private UserContextMonitoringController userContextMonitoringController;
 	public static boolean isLoggedIn = false;
 
 	@Override
@@ -67,18 +80,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		switch(v.getId()) {
 		case R.id.login_list_button:
 			topLayout.removeAllViews();
-			loginView = new LoginView(context, this);
+			loginView = new LoginView(context);
 			topLayout.addView(loginView);
 			break;
 		case R.id.security_info_list_button:
-			if (LoginView.isLoggedIn) {
+			if (isLoggedIn) {
 				topLayout.removeAllViews();
 				securityInformationView = new SecurityInformationView(context);
 				topLayout.addView(securityInformationView);
 			}
 			break;
 		case R.id.policy_info_list_button:
-			if (LoginView.isLoggedIn) {
+			if (isLoggedIn) {
 				topLayout.removeAllViews();
 				privacyPolicyView = new PrivacyPolicyView(context);
 				topLayout.addView(privacyPolicyView);
@@ -92,14 +105,64 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onResume();       
     }
 
+    
+	@SuppressLint("HandlerLeak")
+	private Handler callbackHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch(msg.what){
+			case MusesUICallbacksHandler.LOGIN_SUCCESSFUL:
+				LoginView view = new LoginView(context);
+				view.updateLoginView();
+				isLoggedIn=true;
+				toastMessage(getResources().getString(R.string.login_success_msg));
+				showResultDialog(msg.getData().getString("message"), MusesUICallbacksHandler.LOGIN_SUCCESSFUL);
+
+				break;
+			case MusesUICallbacksHandler.LOGIN_UNSUCCESSFUL:
+				toastMessage(getResources().getString(R.string.login_fail_msg));
+				break;
+			case MusesUICallbacksHandler.ACTION_RESPONSE_ACCEPTED:
+				Log.d(TAG, "Action response accepted ..");
+				showResultDialog(msg.getData().getString("message"), MusesUICallbacksHandler.ACTION_RESPONSE_ACCEPTED);
+				break;
+			case MusesUICallbacksHandler.ACTION_RESPONSE_DENIED:
+				Log.d(TAG, "Action response denied ..");
+				showResultDialog(msg.getData().getString("message"), MusesUICallbacksHandler.ACTION_RESPONSE_DENIED);
+				break;
+			case MusesUICallbacksHandler.ACTION_RESPONSE_MAY_BE:
+				Log.d(TAG, "Action response maybe ..");
+				showResultDialog(msg.getData().getString("message"), MusesUICallbacksHandler.ACTION_RESPONSE_MAY_BE);
+				break;
+			case MusesUICallbacksHandler.ACTION_RESPONSE_UP_TO_USER:
+				Log.d(TAG, "Action response upToUser ..");
+				showResultDialog(msg.getData().getString("message"), MusesUICallbacksHandler.ACTION_RESPONSE_UP_TO_USER);
+				break;
+
+			}
+		}
+
+	};
+
+	
+	/**
+	 * Send user's decision back to MusDM which will either allow MusesAwareApp or not
+	 * @param action
+	 */
+	
+	private void sendUserDecisionToMusDM(Action action){
+		//userContextMonitoringController.sendUserAction(action,null);
+	}
+	
 	/**
 	 * Shows the result dialog to the user
 	 * @param message
 	 */
 	
-	public void showResultDialog(String message){
+	public void showResultDialog(String message, int type ){
 		Log.d(TAG, "Showing result dialog");
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this)
+		AlertDialog.Builder builder = new AlertDialog.Builder(this)
 		.setTitle("MUSES Policy Warning")
 		.setMessage(message)
 		.setCancelable(true)
@@ -107,18 +170,158 @@ public class MainActivity extends Activity implements View.OnClickListener {
 			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				
+				Action action = new Action();
+				action.setType(ActionType.OK);
+				action.setTimestamp(System.currentTimeMillis());
+				sendUserDecisionToMusDM(action); 
 			}
 		})
 		.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				
+				Action action = new Action();
+				action.setType(ActionType.CANCEL);
+				action.setTimestamp(System.currentTimeMillis());
+				sendUserDecisionToMusDM(action); 
 			}
 		});
+		AlertDialog alertDialog = builder.create();
 		alertDialog.show();								
 		
+		if(type == MusesUICallbacksHandler.ACTION_RESPONSE_DENIED) {
+			alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+		}
 	}	
-    
+
+	/**
+	 * Toast messages to UI
+	 * @param message
+	 */
+	
+	private void toastMessage(String message) {
+		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+	}
+		
+	/**
+	 * Check the login fields and registers for callbacks using MusesUICallbacksHandler
+	 * in UserContextMonitoringImplementation. Then tries login to the server
+	 */
+	
+	public void registerCallbacksAndLogin(String userName, String password) {
+		if (checkLoginInputFields(userName, password)){
+			MusesUICallbacksHandler musesUICallbacksHandler = new MusesUICallbacksHandler(context, callbackHandler);
+			userContextMonitoringController.registerCallback(musesUICallbacksHandler);
+			userContextMonitoringController.login(userName, password);
+		}else {
+			toastMessage(getResources().getString(R.string.empty_login_fields_msg));
+		}
+	}
+	
+	/**
+	 * Check input fields are not empty before sending it for authentication
+	 * @param userName
+	 * @param password
+	 * @return
+	 */
+	
+	private boolean checkLoginInputFields(String userName, String password) {
+		if (userName.equals("") || password.equals("")) { // FIXME need some moew checking in future
+			return false;
+		}	
+		return true;
+	}
+
+	
+	
+	/**
+	 * LoginView class handles Login GUI (Username, passwords etc ) on the main GUI
+	 * 
+	 * @author Yasir Ali
+	 * @version Jan 27, 2014
+	 */
+
+	private class LoginView extends LinearLayout implements View.OnClickListener, OnCheckedChangeListener {
+
+		private EditText userNameTxt,passwordTxt;
+		private LinearLayout loginLayout1, loginLayout2;
+		private Button loginBtn, logoutBtn;
+		private TextView loginDetailTextView;
+		private CheckBox rememberCheckBox, agreeTermsCheckBox;
+		private String userName, password;
+		
+		public LoginView(Context context) {
+			super(context);
+			inflate(context, R.layout.login_view, this);
+			userNameTxt = (EditText)findViewById(R.id.username_text);
+			passwordTxt = (EditText)findViewById(R.id.pass_text);
+			loginDetailTextView = (TextView)findViewById(R.id.login_detail_text_view);
+			rememberCheckBox = (CheckBox)findViewById(R.id.remember_checkbox);
+			rememberCheckBox.setOnCheckedChangeListener(this);
+			agreeTermsCheckBox = (CheckBox)findViewById(R.id.agree_terms_checkbox);
+			agreeTermsCheckBox.setOnCheckedChangeListener(this);
+			loginLayout1 = (LinearLayout) findViewById(R.id.login_layout_1);
+			loginLayout2 = (LinearLayout) findViewById(R.id.login_layout_2);
+			loginBtn = (Button)findViewById(R.id.login_button);
+			loginBtn.setOnClickListener(this);
+			logoutBtn = (Button)findViewById(R.id.logout_button);
+			logoutBtn.setOnClickListener(this);
+			userContextMonitoringController = UserContextMonitoringController.getInstance(context);
+			populateLoggedInView();
+		}
+		
+		/**
+		 * Populate logged in view if user is successfully logged in.
+		 * 
+		 */
+		
+		private void populateLoggedInView() {
+			if (isLoggedIn) {
+				loginLayout2.setVisibility(View.GONE);
+				logoutBtn.setVisibility(View.VISIBLE);
+			}
+		}
+
+
+		@Override
+		public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
+			
+		}
+
+		/**
+		 * Handles all the button on the screen, overridden method for onClickLitsener
+		 * @param View
+		 * 
+		 */
+		
+		@Override
+		public void onClick(View v) {
+			switch(v.getId()) {
+			case R.id.login_button:
+				userName = userNameTxt.getText().toString();
+				password = passwordTxt.getText().toString();
+				registerCallbacksAndLogin(userName, password);
+				break;
+			case R.id.logout_button:
+				logoutBtn.setVisibility(View.GONE);
+				loginDetailTextView.setText(getResources().getString(R.string.login_detail_view_txt));
+				loginLayout2.setVisibility(View.VISIBLE);
+				toastMessage(getResources().getString(R.string.logout_successfully_msg));
+				isLoggedIn=false;
+				break;
+			}
+			
+		}
+		
+	    public void updateLoginView(){
+			loginLayout2.setVisibility(View.GONE);
+			logoutBtn.setVisibility(View.VISIBLE);
+			loginDetailTextView.setText(String.format("%s %s", getResources().getString(R.string.logged_in_info_txt), userName));
+	    }
+	
+	}
+
+	
+	
+	
 }
--- src/eu/musesproject/client/ui/MusesUICallbacksHandler.java
+++ src/eu/musesproject/client/ui/MusesUICallbacksHandler.java
@@ -14,6 +14,13 @@ public class MusesUICallbacksHandler implements IUserContextMonitoringController
 	private static final String TAG = MusesUICallbacksHandler.class.getSimpleName();
 	private Context context;
 	private Handler mHandler;
+	// CallBack messages
+	public static final int LOGIN_SUCCESSFUL = 0;
+	public static final int LOGIN_UNSUCCESSFUL = 1;
+	public static final int ACTION_RESPONSE_ACCEPTED = 2;
+	public static final int ACTION_RESPONSE_DENIED = 3;
+	public static final int ACTION_RESPONSE_MAY_BE = 4;
+	public static final int ACTION_RESPONSE_UP_TO_USER = 5;
 	
 	public MusesUICallbacksHandler(Context context, Handler handler) {
 		this.context = context;
@@ -24,11 +31,11 @@ public class MusesUICallbacksHandler implements IUserContextMonitoringController
 	public void onLogin(boolean result) {
 		Log.d(TAG, "onLogin result: " + result);
 		if (result) {
-			Message msg = mHandler.obtainMessage(LoginView.LOGIN_SUCCESSFUL);
+			Message msg = mHandler.obtainMessage(LOGIN_SUCCESSFUL);
 			mHandler.sendMessage(msg);	
 			
 		} else {
-			Message msg = mHandler.obtainMessage(LoginView.LOGIN_UNSUCCESSFUL);
+			Message msg = mHandler.obtainMessage(LOGIN_UNSUCCESSFUL);
 			mHandler.sendMessage(msg);	
 		}
 	}
@@ -36,7 +43,7 @@ public class MusesUICallbacksHandler implements IUserContextMonitoringController
 	@Override
 	public void onAccept(RiskTreatment riskTreatment) {
 		Log.d(TAG, "onAccept: " + riskTreatment.toString());
-    	Message msg = mHandler.obtainMessage(LoginView.ACTION_ACCEPTED);
+    	Message msg = mHandler.obtainMessage(ACTION_RESPONSE_ACCEPTED);
 		Bundle bundle = new Bundle();
 		bundle.putString("message",riskTreatment.toString());
 		msg.setData(bundle);
@@ -46,7 +53,27 @@ public class MusesUICallbacksHandler implements IUserContextMonitoringController
 	@Override
 	public void onDeny(RiskTreatment riskTreatment) {
 		Log.d(TAG, "onDeny: " + riskTreatment.toString());
-    	Message msg = mHandler.obtainMessage(LoginView.ACTION_DENIED);
+    	Message msg = mHandler.obtainMessage(ACTION_RESPONSE_DENIED);
+		Bundle bundle = new Bundle();
+		bundle.putString("message",riskTreatment.toString());
+		msg.setData(bundle);
+		mHandler.sendMessage(msg);
+	}
+
+	@Override
+	public void onMaybe(RiskTreatment riskTreatment) {
+		Log.d(TAG, "onMaybe: " + riskTreatment.toString());
+    	Message msg = mHandler.obtainMessage(ACTION_RESPONSE_MAY_BE);
+		Bundle bundle = new Bundle();
+		bundle.putString("message",riskTreatment.toString());
+		msg.setData(bundle);
+		mHandler.sendMessage(msg);
+	}
+
+	@Override
+	public void onUpToUser(RiskTreatment riskTreatment) {
+		Log.d(TAG, "onUpToUser: " + riskTreatment.toString());
+    	Message msg = mHandler.obtainMessage(ACTION_RESPONSE_UP_TO_USER);
 		Bundle bundle = new Bundle();
 		bundle.putString("message",riskTreatment.toString());
 		msg.setData(bundle);
