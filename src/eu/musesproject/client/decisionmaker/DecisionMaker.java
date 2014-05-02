d9698b5e6de9a411d28fe9a9706bc6b44269ac26
szamarripa <szamarripa@s2grupo.es>
2014-05-02 13:54:01 +0200

    Addition of dummy RiskTreatments to test the client

--- src/eu/musesproject/client/decisionmaker/DecisionMaker.java
+++ src/eu/musesproject/client/decisionmaker/DecisionMaker.java
@@ -11,6 +11,8 @@ import eu.musesproject.client.model.decisiontable.ActionType;
 import eu.musesproject.client.model.decisiontable.Decision;
 import eu.musesproject.client.model.decisiontable.Request;
 import eu.musesproject.contextmodel.ContextEvent;
+import eu.musesproject.server.risktrust.RiskCommunication;
+import eu.musesproject.server.risktrust.RiskTreatment;
 
 /**
  * The Class LocalPolicySelector.
@@ -53,23 +55,35 @@ public class DecisionMaker {
 
         Decision decision = new Decision();
 
+        RiskCommunication riskCommunication = new RiskCommunication();
+        RiskTreatment riskTreatment = null;
+        RiskTreatment[] arrayTreatment = new RiskTreatment[]{riskTreatment};
+        
         if(request.getAction() != null) {
             if (request.getAction().getActionType().equals(ActionType.ACCESS)){
                 decision.setName(Decision.GRANTED_ACCESS);
+                riskTreatment = new RiskTreatment("No additional treatment is needed");//TODO: Use riskTreatments stored in the local database                                
             }else if (request.getAction().getActionType().equals(ActionType.OPEN)){
                 decision.setName(Decision.MAYBE_ACCESS_WITH_RISKTREATMENTS);
+                riskTreatment = new RiskTreatment("Requested action will be allowed with the user connects to an encrypted Wi-Fi");//TODO: Use riskTreatments stored in the local database                
             }else if (request.getAction().getActionType().equals(ActionType.RUN)){
                 decision.setName(Decision.STRONG_DENY_ACCESS);
+                riskTreatment = new RiskTreatment("Requested action is not allowed, no matter the settings");//TODO: Use riskTreatments stored in the local database
             }else if (request.getAction().getActionType().equals(ActionType.INSTALL)){
                 decision.setName(Decision.UPTOYOU_ACCESS_WITH_RISKCOMMUNICATION);
+                riskTreatment = new RiskTreatment("This action is potentially unsecure.You might continue with the action, under your own risk.");//TODO: Use riskTreatments stored in the local database
             } else {
-                return null;
+            	decision.setName(Decision.STRONG_DENY_ACCESS);
+                riskTreatment = new RiskTreatment("Requested action is not allowed, no matter the settings");//TODO: Use riskTreatments stored in the local database
             }
         }
         else {
-            return null;
+        	decision.setName(Decision.STRONG_DENY_ACCESS);
+            riskTreatment = new RiskTreatment("Requested action is not allowed, no matter the settings");//TODO: Use riskTreatments stored in the local database
         }
-				
+		
+        riskCommunication.setRiskTreatment(arrayTreatment);
+        decision.setRiskCommunication(riskCommunication);
 		return decision;
 
 	}	
