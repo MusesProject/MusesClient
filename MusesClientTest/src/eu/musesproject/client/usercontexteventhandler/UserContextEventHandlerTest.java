c1c8792dae7d87410f0f707f46f2e5da035ab6fe
Christoph <christophstanik@gmail.com>
2014-05-28 19:21:48 +0200

    removed JUnit faillure

--- MusesClientTest/src/eu/musesproject/client/usercontexteventhandler/UserContextEventHandlerTest.java
+++ MusesClientTest/src/eu/musesproject/client/usercontexteventhandler/UserContextEventHandlerTest.java
@@ -1,26 +1,26 @@
 package eu.musesproject.client.usercontexteventhandler;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.test.AndroidTestCase;
-import android.test.RenamingDelegatingContext;
 import eu.musesproject.client.contextmonitoring.sensors.AppSensor;
 import eu.musesproject.client.contextmonitoring.sensors.ConnectivitySensor;
 import eu.musesproject.client.db.entity.Property;
 import eu.musesproject.client.db.handler.DBManager;
 import eu.musesproject.contextmodel.ContextEvent;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 public class UserContextEventHandlerTest extends AndroidTestCase {
     private DBManager dbManager;
-    private RenamingDelegatingContext context;
+    private Context context;
     private UserContextEventHandler handler;
     private List<ContextEvent> contextEvents;
     private final long timestamp = System.currentTimeMillis();
 
     public void setUp(){
-        context = new RenamingDelegatingContext(getContext(), "test_");
+        context = getContext();
         dbManager = new DBManager(context);
         handler = UserContextEventHandler.getInstance();
         handler.setContext(getContext());
@@ -51,26 +51,27 @@ public class UserContextEventHandlerTest extends AndroidTestCase {
         List<eu.musesproject.client.db.entity.ContextEvent> storedContextEvents = dbManager.getAllStoredContextEvents();
 
         assertEquals("no of stored context events", 2, storedContextEvents.size());
+        
 
         // connectivity context event
         eu.musesproject.client.db.entity.ContextEvent connectivityContextEvent = storedContextEvents.get(0);
-        assertEquals("Connectivity context event", timestamp, connectivityContextEvent.getTimestamp());
+        assertEquals("Connectivity context event", String.valueOf(timestamp), connectivityContextEvent.getTimestamp());
         assertEquals("Connectivity context event", ConnectivitySensor.TYPE, connectivityContextEvent.getType());
-        List<Property> connectivityProperties = dbManager.getAllProperties();
+        List<Property> connectivityProperties = dbManager.getPropertiesOfContextEvent(storedContextEvents.get(0).getId());
         assertEquals("number of connectivity properties", 3, connectivityProperties.size());
-        assertEquals("connectivity property: wifiEnabled", "wifiEnabled", connectivityProperties.get(0).getKey());
-        assertEquals("connectivity property: wifiEnabled", "true", connectivityProperties.get(0).getValue());
-        assertEquals("connectivity property: wifiConnected", "wifiConnected", connectivityProperties.get(1).getKey());
-        assertEquals("connectivity property: wifiConnected", "true", connectivityProperties.get(1).getValue());
+        assertEquals("connectivity property: wifiConnected", "wifiConnected", connectivityProperties.get(0).getKey());
+        assertEquals("connectivity property: wifiConnected", "true", connectivityProperties.get(0).getValue());
+        assertEquals("connectivity property: wifiEnabled", "wifiEnabled", connectivityProperties.get(1).getKey());
+        assertEquals("connectivity property: wifiEnabled", "true", connectivityProperties.get(1).getValue());
         assertEquals("connectivity property: airplaneMode", "airplaneMode", connectivityProperties.get(2).getKey());
         assertEquals("connectivity property: airplaneMode", "false", connectivityProperties.get(2).getValue());
 
 
         // app context event
-        eu.musesproject.client.db.entity.ContextEvent appContextEvent = storedContextEvents.get(2);
-        assertEquals("app context event", timestamp, appContextEvent.getTimestamp());
+        eu.musesproject.client.db.entity.ContextEvent appContextEvent = storedContextEvents.get(1);
+        assertEquals("app context event", String.valueOf(timestamp), appContextEvent.getTimestamp());
         assertEquals("app context event", AppSensor.TYPE, appContextEvent.getType());
-        List<Property> appProperties = dbManager.getAllProperties();
+        List<Property> appProperties = dbManager.getPropertiesOfContextEvent(storedContextEvents.get(1).getId());
         assertEquals("number of app properties", 1, appProperties.size());
         assertEquals("connectivity property: appName", "appName", appProperties.get(0).getKey());
         assertEquals("connectivity property: appName", "gmail", appProperties.get(0).getValue());
