d3f18489da29820413ff7b74dbf212a7c76c0352
yasali <yasir.ali@swedenconnectivity.com>
2014-06-30 15:59:36 +0200

    License headers added in Muses Client (Connection Manager, UI)

--- src/eu/musesproject/client/connectionmanager/ConnectionManager.java
+++ src/eu/musesproject/client/connectionmanager/ConnectionManager.java
@@ -1,10 +1,23 @@
-/*
- * version 1.0 - MUSES prototype software
- * Copyright MUSES project (European Commission FP7) - 2013 
- * 
- */
 package eu.musesproject.client.connectionmanager;
-
+/*
+ * #%L
+ * MUSES Client
+ * %%
+ * Copyright (C) 2013 - 2014 Sweden Connectivity
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 import java.io.IOException;
 
 import org.apache.http.HttpResponse;
