60700c5f95bd8bfad2dff846bb42d535bbec1199
Christoph Stanik <christoph.stanik.developer@gmail.com>
2014-08-05 11:10:08 +0200

    created JUnit test for method: createLoginJSON in class: JSONManager

--- MusesClientTest/src/eu/musesproject/client/contextmonitoring/test/JSONManagerTest.java
+++ MusesClientTest/src/eu/musesproject/client/contextmonitoring/test/JSONManagerTest.java
@@ -8,8 +8,11 @@ import eu.musesproject.client.model.decisiontable.ActionType;
 import eu.musesproject.client.usercontexteventhandler.JSONManager;
 import eu.musesproject.contextmodel.ContextEvent;
 import junit.framework.TestCase;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,9 +29,11 @@ public class JSONManagerTest extends TestCase {
     
     private String deviceId;
     private String userName; 
+    private String password; 
     private String requestType;
     private long actionTimestamp;
     private String actionType;
+    private String userBehavior;
 
     @Override
     protected void setUp() throws Exception {
@@ -39,6 +44,7 @@ public class JSONManagerTest extends TestCase {
 
         deviceId = "123456";
         userName = "muses";
+        password = "muses";
         
         actionTimestamp = System.currentTimeMillis();
         actionType = ActionType.ACCESS;
@@ -61,6 +67,8 @@ public class JSONManagerTest extends TestCase {
 
         contextEvents = new ArrayList<ContextEvent>();
         contextEvents.add(contextEvent);
+        
+        userBehavior = ActionType.CANCEL;
     }
 
     public void testCreateJSON() throws JSONException {
@@ -70,20 +78,48 @@ public class JSONManagerTest extends TestCase {
         assertEquals("request type", "update_context_events", resultJSON.getString(JSONIdentifiers.REQUEST_TYPE_IDENTIFIER));
 
         JSONObject actionJSON = resultJSON.getJSONObject(JSONIdentifiers.ACTION_IDENTIFIER);
+        assertNotNull(actionJSON);
         assertEquals("action identifier in JSON :", actionType, actionJSON.getString(JSONIdentifiers.ACTION_TYPE));
         assertEquals("action timestamp:" , String.valueOf(actionTimestamp), actionJSON.getString(JSONIdentifiers.ACTION_TIMESTAMP));
 
         JSONObject actionPropertiesJSON = actionJSON.getJSONObject(JSONIdentifiers.PROPERTIES_IDENTIFIER);
+        assertNotNull(actionPropertiesJSON);
         assertEquals("property: protocol","https", actionPropertiesJSON.getString("protocol"));
         assertEquals("property: url","https://", actionPropertiesJSON.getString("url"));
         assertEquals("property: resourceid","file.png", actionPropertiesJSON.getString("resourceid"));
         assertEquals("property: method","post", actionPropertiesJSON.getString("method"));
 
         JSONObject sensorJSON = resultJSON.getJSONObject(JSONIdentifiers.SENSOR_IDENTIFIER);
+        assertNotNull(sensorJSON);
         JSONObject appSensorJSONObject = sensorJSON.getJSONObject(AppSensor.TYPE);
+        assertNotNull(appSensorJSONObject);
         assertEquals("app name:" , "app", appSensorJSONObject.getString(AppSensor.PROPERTY_KEY_APP_NAME));
         assertEquals("app id:" , "1", appSensorJSONObject.getString(AppSensor.PROPERTY_KEY_ID));
         assertEquals("app background processes:" , "process1,process2,process3", appSensorJSONObject.getString(AppSensor.PROPERTY_KEY_BACKGROUND_PROCESS));
+    }
+    
+    public void testCreateUserBehaviorJSON() throws JSONException {
+    	JSONObject resultJSON = JSONManager.createUserBehaviorJSON(deviceId, userName, userBehavior);
+        assertNotNull(resultJSON);
+        Log.d("TEST", "JSON MESSAGE: " + resultJSON.toString());
+
+    	assertEquals("request type", RequestType.USER_ACTION, resultJSON.getString(JSONIdentifiers.REQUEST_TYPE_IDENTIFIER));
+    	assertEquals("device id", deviceId, resultJSON.getString(JSONIdentifiers.AUTH_DEVICE_ID));
+        assertEquals("user name", userName, resultJSON.getString(JSONIdentifiers.AUTH_USERNAME));
+        
+        JSONObject userBehaviorJSON = resultJSON.getJSONObject(JSONIdentifiers.USER_BEHAVIOR);
+        assertNotNull(userBehaviorJSON);
+        assertEquals("user behavior", userBehavior, userBehaviorJSON.getString(JSONIdentifiers.ACTION_IDENTIFIER));
+    }
+    
+    public void testCreateLoginJSON() throws JSONException {
+    	JSONObject resultJSON = JSONManager.createLoginJSON(userName, password, deviceId);
+    	assertNotNull(resultJSON);
+ 
+    	assertEquals("request type", RequestType.LOGIN, resultJSON.getString(JSONIdentifiers.REQUEST_TYPE_IDENTIFIER));
+    	assertEquals("device id", deviceId, resultJSON.getString(JSONIdentifiers.AUTH_DEVICE_ID));
+        assertEquals("password", password, resultJSON.getString(JSONIdentifiers.AUTH_PASSWORD));
+        assertEquals("userName", userName, resultJSON.getString(JSONIdentifiers.AUTH_USERNAME));
     }
 
     @Override
