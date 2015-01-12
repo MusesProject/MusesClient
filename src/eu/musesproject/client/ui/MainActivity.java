84f54702cf4b18409572edc4ffe3f131bff552aa
Christoph Stanik <christoph.stanik.developer@gmail.com>
2015-01-12 13:50:25 +0100

    hide the app name in the action bar

--- src/eu/musesproject/client/ui/MainActivity.java
+++ src/eu/musesproject/client/ui/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		setContentView(R.layout.muses_main);
 		context = getApplicationContext();
 		setStartUpConfiguration();
+		getActionBar().setDisplayShowTitleEnabled(false);
 		
 		topLayout = (LinearLayout) findViewById(R.id.top_layout);
 		loginListBtn = (Button) findViewById(R.id.login_list_button);
