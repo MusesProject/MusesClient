0758e4c4cdad8d163a013c7652529ace39dc93d1
Christoph <christophstanik@gmail.com>
2015-01-26 14:03:18 +0100

    Update UserContextEventHandler.java

--- src/eu/musesproject/client/usercontexteventhandler/UserContextEventHandler.java
+++ src/eu/musesproject/client/usercontexteventhandler/UserContextEventHandler.java
@@ -673,16 +673,21 @@ public class UserContextEventHandler implements RequestTimeoutTimer.RequestTimeo
 
 
 	public String getUserName() {
-		if(prefs == null) {
-			prefs = context.getSharedPreferences(MainActivity.PREFERENCES_KEY,
-					Context.MODE_PRIVATE);
-		}
-		this.userName = prefs.getString(MainActivity.USERNAME, "");
-		if(userName == null || userName.equals("")) {
-			this.userName = "unknown"; // TODO look in db too
-		}
+		if(isUserAuthenticated) {
+			if (prefs == null) {
+				prefs = context.getSharedPreferences(MainActivity.PREFERENCES_KEY,
+						Context.MODE_PRIVATE);
+			}
+			this.userName = prefs.getString(MainActivity.USERNAME, "");
+			if (userName == null || userName.equals("")) {
+				this.userName = "unknown"; // TODO look in db too
+			}
 
-		return userName;
+			return userName;
+		}
+		else {
+			return "unknown";
+		}
 	}
 
 	public void removeRequestById(int requestId) {
